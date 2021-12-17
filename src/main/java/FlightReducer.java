fa630f6d94d727916069ed2f87ab7d351f3077bd
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 02:59:24 +0300

    Scripted auto-commit on change (2021-12-18 02:59:24) by gitwatch.sh

--- src/main/java/FlightReducer.java
+++ src/main/java/FlightReducer.java
@@ -1,2 +1,6 @@
-public class FlightReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class FlightReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
+    
 }
