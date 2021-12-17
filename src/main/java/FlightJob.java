450976e27678dca590d0cdc6dd3cb5e74e8296db
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 02:34:09 +0300

    Scripted auto-commit on change (2021-12-18 02:34:09) by gitwatch.sh

--- src/main/java/FlightJob.java
+++ src/main/java/FlightJob.java
@@ -1,5 +1,18 @@
 public class FlightJob {
     public static void main(String[] args) throws Exception {
-        
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
