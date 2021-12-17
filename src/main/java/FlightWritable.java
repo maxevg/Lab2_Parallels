921b7c151cf753dd1beb858046120bfa33f0e08b
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 01:42:26 +0300

    Scripted auto-commit on change (2021-12-18 01:42:26) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -8,6 +8,14 @@ public class FlightWritable implements Writable {
     private float CANCELLED;
 
     public FlightWritable() {}
-    
+
+    public FlightWritable(int DEST_AIRPORT_ID, float ARR_DELAY, float AIR_TIME, float CANCELLED) {
+        this.DEST_AIRPORT_ID = DEST_AIRPORT_ID;
+        this.ARR_DELAY = ARR_DELAY;
+        this.AIR_TIME = AIR_TIME;
+        this.CANCELLED = CANCELLED;
+    }
+
+
 
 }
