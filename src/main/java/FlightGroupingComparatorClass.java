a3e278d18052bcd54e080e3b9e4f9f55bf34aad1
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 02:46:18 +0300

    Scripted auto-commit on change (2021-12-18 02:46:18) by gitwatch.sh

--- src/main/java/FlightGroupingComparatorClass.java
+++ src/main/java/FlightGroupingComparatorClass.java
@@ -1,7 +1,12 @@
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FlightGroupingComparatorClass extends WritableComparator {
     protected  FlightGroupingComparatorClass() {
         super(FlightWritableComparable.class, true);
+    }
+
+    public int compare(WritableComparable a, WritableComparable b) {
+        return a.compareTo(b);
     }
 }
