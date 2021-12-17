ff3086ed58dc507551a864b8a93420209163fa82
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 01:37:54 +0300

    Scripted auto-commit on change (2021-12-18 01:37:54) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -3,6 +3,10 @@ import org.apache.hadoop.io.Writable;
 
 public class FlightWritable implements Writable {
     private int DEST_AIRPORT_ID;
-    
+    private float ARR_DELAY;
+    private float AIR_TIME;
+    private float CANCELLED;
+
+    public FlightWritable() {}
 
 }
