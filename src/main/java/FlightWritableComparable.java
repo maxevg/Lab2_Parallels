85810a635ba57fc559524f303ddae75a28d1e49f
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 01:57:12 +0300

    Scripted auto-commit on change (2021-12-18 01:57:12) by gitwatch.sh

--- src/main/java/FlightWritableComparable.java
+++ src/main/java/FlightWritableComparable.java
@@ -1,8 +1,9 @@
-import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class FlightWritableComparable {
+public class FlightWritableComparable implements WritableComparable {
+    private int 
 }
