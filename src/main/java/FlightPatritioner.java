f92bf2470384c2d3b893215a0a89e73a641a37d3
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 02:18:39 +0300

    Scripted auto-commit on change (2021-12-18 02:18:39) by gitwatch.sh

--- src/main/java/FlightPatritioner.java
+++ src/main/java/FlightPatritioner.java
@@ -1,2 +1,5 @@
-public class FlightPatritioner {
+public class HashPartitioner<K, V> extends Partitioner<K, V> {
+    public int getPartition(K key, V value, int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
 }
