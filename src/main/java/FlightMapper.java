85e90db03b04b375c3c3fa9e882ffc6c83c4316a
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:32:49 +0300

    Scripted auto-commit on change (2021-12-18 03:32:49) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -5,13 +5,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
-
+    private static float CheckNullDelay(String current) {
+        if ()
+    }
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException {
         if (key.get() > 0) {
             String[] Table = value.toString().split(",");
-            int DestAeroportID = Integer.parseInt(table[14]);
+            int DestAeroportID = Integer.parseInt(Table[14]);
+            float ArrDelay = CheckNullDelay(Table[17]);
+
         }
     }
 }
