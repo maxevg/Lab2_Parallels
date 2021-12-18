f33e7156259c3ceeeaae79e57f27348b116551ec
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:46:20 +0300

    Scripted auto-commit on change (2021-12-18 03:46:20) by gitwatch.sh

--- src/main/java/FlightNameMapper.java
+++ src/main/java/FlightNameMapper.java
@@ -2,7 +2,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 
+import java.io.IOException;
+
 
 public class FlightNameMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
-
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() > 0) {
+            
+        }
+    }
 }
