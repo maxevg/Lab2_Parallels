1c3b5a38b84ef9c7596b034e37570453779d2a06
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:59:51 +0300

    Scripted auto-commit on change (2021-12-18 03:59:50) by gitwatch.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ mvn package
 hadoop fs -copyFromLocal 664600583_T_ONTIME_sample.csv
 hadoop fs -copyFromLocal L_AIRPORT_ID.csv
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+hadoop FlightJob
