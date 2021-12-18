312ba7166317c629be2690b6314e37fbedc9d4dc
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:09:49 +0300

    Scripted auto-commit on change (2021-12-18 03:09:49) by gitwatch.sh

--- src/main/java/FlightReducer.java
+++ src/main/java/FlightReducer.java
@@ -1,6 +1,27 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 public class FlightReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
-    protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context)
+    protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text NameInfo = new Text(iter.next());
+        int Count = 0;
+        float Min = 0.0F;
+        float Max = 0.0F;
+        float Average = 0.0F;
+        while (iter.hasNext()) {
+            float CurrentValue = Float.parseFloat(iter.next().toString());
+            if (Count == 0) {
+                Min = CurrentValue;
+            }
+            if (CurrentValue < Min) {
+                Min = CurrentValue;
+            } else if (CurrentValue > Max) {
+                Max = CurrentValue;
+            }
+        }
+    }
 }
