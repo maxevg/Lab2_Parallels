56e2b42024db07babd96ee7c27607de4cf5075cf
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:45:37 +0300

    Scripted auto-commit on change (2021-12-18 03:45:37) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -12,7 +12,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
             return Float.parseFloat(current);
         }
     }
-    
+
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
             String[] Table = value.toString().split(",");
