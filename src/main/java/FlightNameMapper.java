1b5cd8d591b9ff074929d26c21f0153eb115efb4
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:45:29 +0300

    Scripted auto-commit on change (2021-12-18 03:45:29) by gitwatch.sh

--- src/main/java/FlightNameMapper.java
+++ src/main/java/FlightNameMapper.java
@@ -1,2 +1,8 @@
-public class FlightNameMapper {
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+
+
+public class FlightNameMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
+
 }
