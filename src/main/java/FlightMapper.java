6f3905e67a11f3e14384f481384a0339c692e817
Evgeniy <maximov.evgen@gmail.com>
2021-12-18 03:24:38 +0300

    Scripted auto-commit on change (2021-12-18 03:24:38) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,2 +1,18 @@
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
+
+
+    @Override
+    public void map(Text key, TupleWritable value,
+                    OutputCollector<Text, Text> output,
+                    Reporter reporter) throws IOException {
+        Text call = (Text) value.get(0);
+        Text system = (Text) value.get(1);
+        output.collect(call, system);
+    }
 }
